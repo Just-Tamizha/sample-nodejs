f8dd837d70f0c0387bea2e2e48a4fe87b1538f4b
Just-Tamizha <you@example.com>
2024-05-25 20:02:30 +0530

    V.1

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 8000
+app.get("/",(req, res) => {
+    res.send("Welcome to Tamizha github repo!")
+})
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
