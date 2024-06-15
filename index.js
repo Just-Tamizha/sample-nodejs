103293bb727edfb4cab24922edcdc2150c96b922
Just-Tamizha <85663828+Just-Tamizha@users.noreply.github.com>
2024-06-15 20:02:10 +0530

    1.1

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 8000
 app.get("/",(req, res) => {
     res.send("Welcome to Tamizha github repo!")
 })
+app.get("/demo",(req, res) => {
+    res.send("Console")
+})
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 })
